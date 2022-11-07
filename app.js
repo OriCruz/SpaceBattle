38cb10fd09dc19ded8e201def1318713d9de58b3
OriCruz <oc080901@gmail.com>
2022-11-07 18:18:24 -0500

    retreat button almost working how I want, displaying the buttons in the modal when needed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ class AlienShip {
     }
 }
 //#endregion
-//Global variable for the indez of the alien ship array
+//Global variable for the index of the alien ship array
 let index =0;
 //#region 
 const ussShip = {
@@ -16,39 +16,11 @@ const ussShip = {
     firepower: 5,
     accuracy : .7,
     isAlive: true,
-    attack(){
-        if(alienShipFact.shipCollection[index].hullPower>=1){
-            if(Math.random()< ussShip.accuracy)
-            {
-                alienShipFact.shipCollection[index].hullPower -=  ussShip.firepower;
-                updateEnemyStats();
-                return 'You hit the emeny!';
-                }
-                else{
-                return 'You missed! Prepare to get attack'
-                }
-            }
-            else if(alienShipFact.shipCollection[index].hullPower<=0) {
-                //checks if the current index of the array is less or equal to the last one
-                if(alienShipFact.shipCollection.indexOf(alienShipFact.shipCollection[index])<alienShipFact.shipCollection.length-1){
-                    index++;
-                    updateEnemyStats();
-                    //here I should add the retreat function
-
-                    return `You destroyed one alien ship! there are ${alienShipFact.shipCollection.length-1} left`
-                    }
-                    else{
-                        return 'You destroyed all alien ship congrats! Please reload the page to play again'
-                   }
-            } 
-    },
-    retreat(){
-        console.log('this works!');
-    }
     
 }
    
 //#endregion
+
 //#region 
 class AlienShipFactory{
     constructor(){
@@ -70,41 +42,13 @@ class AlienShipFactory{
         this.accuracy = eval((Math.random()*(0.8-0.6)+0.6).toFixed(1));
         return this.accuracy;
     }
-    attack(){
-         if(ussShip.hull>1){
-            if(Math.random()< this.shipCollection[index].accuracy){
-                    ussShip.hull -= alienShipFact.shipCollection[index].firepower;
-                    updatePlayerStats();
-                    return 'You got hit!';
-                
-        }
-        else{
-            return 'Enemy missed! Attack now'
-        }
-    }
-    else if(ussShip.hull<=0) {
-        return 'Game over, aliens have taken over earth! :('
-        }
-    }
 }
 //#endregion
 
+//#region 
 let alienShipFact = new AlienShipFactory();
 alienShipFact.makeNewShips();
 alienShipFact.makeNewShips();
-
-
-function updatePlayerStats (){
-    document.querySelector('.playerStats').innerHTML=`Hull :${ussShip.hull}<br> FirePower : ${ussShip.firepower}<br> Accuracy : ${ussShip.accuracy}`;
-}
-
-function updateEnemyStats() {
-       document.querySelector('.enemyStats').innerHTML=`Hull :${alienShipFact.shipCollection[index].hullPower} <br> FirePower : ${alienShipFact.shipCollection[index].firepower}<br> Accuracy :${alienShipFact.shipCollection[index].accuracy}`;
-}
-//Shows initial stats
-updatePlayerStats();
-updateEnemyStats();
-
 let btn = document.querySelector('.btn');
 let btnAttack = document.querySelector('.btnAttack')
 let modal = document.querySelector('.modal');
@@ -114,6 +58,71 @@ let msg = document.querySelector('#msg');
 let retreatModal = document.querySelector('#retreatBtn');
 let attackModal = document.querySelector('#attackBtn');
 
+//#endregion
+
+function updatePlayerStats (){
+    document.querySelector('.playerStats').innerHTML=`Hull :${ussShip.hull}<br> FirePower : ${ussShip.firepower}<br> Accuracy : ${ussShip.accuracy}`;
+}
+
+function updateEnemyStats() {
+       document.querySelector('.enemyStats').innerHTML=`Hull :${alienShipFact.shipCollection[index].hullPower} <br> FirePower : ${alienShipFact.shipCollection[index].firepower}<br> Accuracy :${alienShipFact.shipCollection[index].accuracy}`;
+}
+
+function attackAliens(){ 
+     if(alienShipFact.shipCollection[index].hullPower>=1){
+    if(Math.random()< ussShip.accuracy)
+    {
+        alienShipFact.shipCollection[index].hullPower -=  ussShip.firepower;
+        updateEnemyStats();
+        return  msg.innerHTML='You hit the emeny!';
+        }
+        else{
+        return  msg.innerHTML='You missed! Prepare to get attack'
+        }
+    }
+    else if(alienShipFact.shipCollection[index].hullPower<=0) {
+        //checks if the current index of the array is less or equal to the last one
+        if(alienShipFact.shipCollection.indexOf(alienShipFact.shipCollection[index])<alienShipFact.shipCollection.length-1){
+            index++;
+            updateEnemyStats();
+            //here I show the buttons to either retreat or keep attacking
+            retreatModal.style.display= 'inline';
+            attackModal.style.display='inline';
+            return  msg.innerHTML=`You destroyed one alien ship! there are ${alienShipFact.shipCollection.length-1} left`
+            }
+            else{
+                modalBtns.style.display='none';
+                return  msg.innerHTML='You destroyed all alien ship congrats! Please reload the page to play again'
+           }
+        }
+}
+
+function retreat(){
+    retreatModal.style.display='none';
+    attackModal.style.display='none';
+    return`you have retreated, GAME OVER COWARD!`
+}
+function attackUssShip(){
+
+        if(ussShip.hull>1){
+           if(Math.random()< alienShipFact.shipCollection[index].accuracy){
+                   ussShip.hull -= alienShipFact.shipCollection[index].firepower;
+                   updatePlayerStats();
+                   return 'You got hit!';
+               
+       }
+       else{
+           return 'Enemy missed! Attack now'
+       }
+   }
+   else if(ussShip.hull<=0) {
+       return 'Game over, aliens have taken over earth! :('
+       }
+}
+
+//Shows initial stats
+updatePlayerStats();
+updateEnemyStats();
 // When the user clicks on the button, open the modal
 btn.addEventListener('click', function(){
     modal.style.display ='block';
@@ -131,17 +140,21 @@ btnAttack.addEventListener('click', function(){
         modal.style.display ='block';
         btnAttack.innerHTML = 'Await Attack';
         //attacks the aliens
-        return msg.innerHTML= ussShip.attack();
+        attackAliens();
+           
     }
     else if(btnAttack.innerHTML !=="Attack"){
        modal.style.display ='block';
        btnAttack.innerHTML = 'Attack';
        //attacks the ussShip
-      return msg.innerHTML= alienShipFact.attack();
+      return msg.innerHTML= attackUssShip();
     }
 })
 
 retreatModal.addEventListener('click', function(){
- ussShip.retreat();
- modal.style.display ='none';
-})
+ 
+ return msg.innerHTML = retreat();
+// })
+// attackModal.addEventListener('click', function(){
+
+// })
